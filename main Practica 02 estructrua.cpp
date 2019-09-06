c4c6177e5a42895f5f5ff972cc8215e7315e8ec2
paosanchezmonroy <paola_sanchez_monroy@hotmail.com>
2019-09-05 23:53:08 -0500

    Modificacion, menu de 10 personas avance, en este menu pide al usuario ingresar 10 personas, todo esto con apuntadores

--- main Practica 02 estructrua.cpp
+++ main Practica 02 estructrua.cpp
@@ -1,57 +1,52 @@
 #include <iostream>
-#include <conio.h>
 
 using namespace std;
-struct comida{
-	char sabor[20];
-	int cantidad;
-	char direccion [20];
-	
-};
 
-struct restaurante{
+struct direccion{
+	char calle[20];
+	int numero;
+	
+}; 
+struct revista{
 	char nombre [20];
-	int mesero;
-	float propina;
-	struct comida comi;
+	int meses;
+	char titulo [20];
+	
+	struct direccion dire;
 };
 
 int main(int argc, char** argv) {
-	struct restaurante R1[6];
-	struct restaurante *apuntador;
+	struct revista R1[6];
+	struct revista *apuntador;
 	apuntador=&R1[0];
 	
-	cout<<".::Bienvenido a estructura::."<<endl;
+	int num;
+	cout<<"\t.::Elija una opcion::.\n"<<endl;
+	cout<<"1. Datos del usuario "<<endl;
+	cout<<"2. Mostrar datos del usuario "<<endl;
+	cout<<"3. Cambiar datos"<<endl;
+	cout<<"4. Salir "<<endl;
+	cin>>num;
 	
-	for (int i=0; i<2; i++){
-		cout<<"Ingresa el nombre del restaurante: "<<endl;
-		cin.getline(R1[i].nombre,20);
-		cout<<"Ingresa el numero de meseros que necesitas: "<<endl;
-		cin>>R1[i].mesero;
-		cout<<"Ingresa la propina que deseas dejar: "<<endl;
-		cin>>R1[i].propina;
-		cout<<"Ingrese el sabor de su comida: "<<endl;
-		fflush(stdin);
-		cin.getline(R1[i].comi.sabor,20);
+ switch (num)
+ {
+ 	case 1:
+	 cout<<"*Opcion 1* "<<endl;
+	 cout<<"Ingresa el nombre de la persona: "<<endl;
+	 fflush(stdin);
+	 cin.getline(R1[0].nombre,20);
+	 cout<<"Ingresa los meses a utilizar: "<<endl;
+	 cin>>R1[0].meses;
+	 cout<<"Ingrese el titular: "<<endl;
+	 cin>>R1[0].titulo;
+	 system("pause");
+		break;
 		
-		system("pause");	
-	}
-	
-	
-	getch();
-	cout<<"***Mostrar los datos guardados***"<<endl;
-	cout<<endl<<"Nombre del restaurante: "<<apuntador->nombre;
-	cout<<endl<<"Numero de meseros: "<<R1[0].mesero;
-	cout<<endl<<"Propina a dejar: "<<R1[0].propina;
-	cout<<endl<<"Sabor de la comida: "<<R1[0].comi.sabor;
-	
-	system("pasue");
-	cout<<"***Mostrar los datos guardados***"<<endl;
-	cout<<endl<<"Nombre del restaurante: "<<apuntador->nombre;
-	cout<<endl<<"Numero de meseros: "<<R1[1].mesero;
-	cout<<endl<<"Propina a dejar: "<<R1[1].propina;
-	cout<<endl<<"Sabor de la comida: "<<R1[1].comi.sabor;
-	
+	case 2:
+		break;
+ 		
+ 		
+ 		
+    }while (num !=4);
 	return 0;
-	
 }
